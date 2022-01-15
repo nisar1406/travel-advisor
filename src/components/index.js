e79aa3bb88ef213af5c6c005638362384747fe7e
Nisar Shaikh <>
2022-01-15 14:36:54 +0530

    code setup

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './header/header';
+export { default as List } from './list/list';
+export { default as Map } from './map/map';
+export { default as PlaceDetails } from './place-details/placeDetails';
